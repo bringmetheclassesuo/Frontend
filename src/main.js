7a6e772059a2f19264fa1a08d4e0d75e1edee6f5
andrewpratheepan <andrew.pratheepan@gmail.com>
2019-07-06 10:37:05 -0400

    project setup

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Antd from 'ant-design-vue'
+import App from './App'
+import 'ant-design-vue/dist/antd.css'
+import Multiselect from 'vue-multiselect'
+
+// register globally
+Vue.component('multiselect', Multiselect)
 
 Vue.config.productionTip = false
+
+Vue.use(Antd)
 
 new Vue({
   render: h => h(App),
