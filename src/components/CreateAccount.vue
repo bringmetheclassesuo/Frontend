e7827d1e9d76afdeb3bb11120e38e2e9e4bd6c07
Bryan Battershill <bryan.battershill@ocsbstudent.ca>
2019-07-14 14:03:59 -0400

    Changed format to actually work lmao

--- src/components/CreateAccount.vue
+++ src/components/CreateAccount.vue
@@ -1,56 +1,107 @@
 <template>
-    <div style="padding-top: 10%">
-        <form class="ant-form ant-form-horizontal" data-v-21b4b419="">
-            <div class="ant-row ant-form-item">
-                <div class="ant-col-xs-24 ant-col-sm-8 ant-form-item-label"><label for="email" title="E-mail" class="ant-form-item-required">E-mail</label></div>
-                <div class="ant-col-xs-24 ant-col-sm-16 ant-form-item-control-wrapper">
-                    <div class="ant-form-item-control"><span class="ant-form-item-children"><input type="text" data-__meta="[object Object]" data-__field="[object Object]" id="email" class="ant-input"></span>
-                        <!---->
-                    </div>
-                </div>
-            </div>
-            <div class="ant-row ant-form-item">
-                <div class="ant-col-xs-24 ant-col-sm-8 ant-form-item-label"><label for="password" title="Password" class="ant-form-item-required">Password</label></div>
-                <div class="ant-col-xs-24 ant-col-sm-16 ant-form-item-control-wrapper">
-                    <div class="ant-form-item-control"><span class="ant-form-item-children"><input type="password" data-__meta="[object Object]" data-__field="[object Object]" id="password" class="ant-input"></span>
-                        <!---->
-                    </div>
-                </div>
-            </div>
-            <div class="ant-row ant-form-item">
-                <div class="ant-col-xs-24 ant-col-sm-8 ant-form-item-label"><label for="confirm" title="Confirm Password" class="ant-form-item-required">Confirm Password</label></div>
-                <div class="ant-col-xs-24 ant-col-sm-16 ant-form-item-control-wrapper">
-                    <div class="ant-form-item-control"><span class="ant-form-item-children"><input type="password" data-__meta="[object Object]" data-__field="[object Object]" id="confirm" class="ant-input"></span>
-                        <!---->
-                    </div>
-                </div>
-            </div>
-    <div class="ant-row ant-form-item">
-        <div class="ant-col-xs-24 ant-col-xs-offset-0 ant-col-sm-16 ant-col-sm-offset-8 ant-form-item-control-wrapper">
-            <div class="ant-form-item-control"><span class="ant-form-item-children"><label class="ant-checkbox-wrapper"><span class="ant-checkbox"><input id="agreement" type="checkbox" class="ant-checkbox-input" value=""><span class="ant-checkbox-inner"></span></span><span>
-          I have read the <a href="">
-            agreement
-          </a></span></label>
-                </span>
-                <!---->
-            </div>
-        </div>
-    </div>
-    <div class="ant-row ant-form-item">
-        <div class="ant-col-xs-24 ant-col-xs-offset-0 ant-col-sm-16 ant-col-sm-offset-8 ant-form-item-control-wrapper">
-            <div class="ant-form-item-control"><span class="ant-form-item-children"><button type="submit" class="ant-btn ant-btn-primary"><span>Register</span></button>
-                </span>
-                <!---->
-            </div>
-        </div>
-    </div>
-    </form>
+    <div style = "padding: 10%">
+        <a-form :form="form" @submit="handleSubmit">
+            <a-form-item v-bind="formItemLayout" label="E-mail">
+                <a-input v-decorator="[
+                'email',
+                {
+                    rules: [{
+                    type: 'email', message: 'The input is not valid E-mail!',
+                    }, {
+                    required: true, message: 'Please input your E-mail!',
+                    }]
+                }
+                ]" />
+            </a-form-item>
+            <a-form-item v-bind="formItemLayout" label="Password">
+                <a-input v-decorator="[
+                'password',
+                {
+                    rules: [{
+                    required: true, message: 'Please input your password!',
+                    }, {
+                    validator: validateToNextPassword,
+                    }],
+                }
+                ]" type="password" />
+            </a-form-item>
+            <a-form-item v-bind="formItemLayout" label="Confirm Password">
+                <a-input v-decorator="[
+                'confirm',
+                {
+                    rules: [{
+                    required: true, message: 'Please confirm your password!',
+                    }, {
+                    validator: compareToFirstPassword,
+                    }],
+                }
+                ]" type="password" @blur="handleConfirmBlur" />
+            </a-form-item>
+            <a-form-item v-bind="tailFormItemLayout">
+                <a-checkbox v-decorator="['agreement', {valuePropName: 'checked'}]">
+                    I have read the <a href="">
+                agreement
+                </a>
+                </a-checkbox>
+            </a-form-item>
+            <a-form-item v-bind="tailFormItemLayout">
+                <a-button type="primary" html-type="submit">
+                    Register
+                </a-button>
+            </a-form-item>
+        </a-form>
     </div>
 </template>
 
+
+</script>
+
 <script>
     export default {
-        name: "CreateAccount"
+        name: "CreateAccount",
+        beforeCreate() {
+            this.form = this.$form.createForm(this);
+        },
+        methods: {
+            handleSubmit(e) {
+                e.preventDefault();
+                this.form.validateFieldsAndScroll((err, values) => {
+                    if (!err) {
+                        console.log('Received values of form: ', values);
+                    }
+                });
+            },
+            handleConfirmBlur(e) {
+                const value = e.target.value;
+                this.confirmDirty = this.confirmDirty || !!value;
+            },
+            compareToFirstPassword(rule, value, callback) {
+                const form = this.form;
+                if (value && value !== form.getFieldValue('password')) {
+                    callback('Two passwords that you enter is inconsistent!');
+                } else {
+                    callback();
+                }
+            },
+            validateToNextPassword(rule, value, callback) {
+                const form = this.form;
+                if (value && this.confirmDirty) {
+                    form.validateFields(['confirm'], {
+                        force: true
+                    });
+                }
+                callback();
+            },
+            handleWebsiteChange(value) {
+                let autoCompleteResult;
+                if (!value) {
+                    autoCompleteResult = [];
+                } else {
+                    autoCompleteResult = ['.com', '.org', '.net'].map(domain => `${value}${domain}`);
+                }
+                this.autoCompleteResult = autoCompleteResult;
+            },
+        },
     }
 </script>
 
